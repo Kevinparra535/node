5c5309c1eed9d33415b19411ab36e7112c350b59
Kevin Parra <kevinparra535@gmail.com>
2021-09-30 10:38:49 -0500

    [UPDATE] Construyendo módulos: Require e Import

---
+++ packages/module/es6/index.mjs
@@ -0,0 +1,12 @@
+// Traer nuestro modulo
+import modulo from "./module.mjs";
+
+// Ejecutar una funcion del modulo
+// modulo();
+
+modulo.saludar();
+console.log(modulo.prop1);
+
+// Entorno experimental de es6
+
+// Para ejecutar este tipo de archivos es node --experimental-modules index.mjs || node index.mjs
---
+++ packages/module/es6/module.mjs
@@ -0,0 +1,9 @@
+function saludar() {
+  console.log("Hola");
+}
+
+// export default saludar;
+export default {
+  saludar,
+  prop1: "valor1",
+};
